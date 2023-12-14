6af0d3806d9f1cad68067995ec45c4437357c53c
sharmahaihum <shashwatprasad66@icloud.com>
2023-12-14 10:31:48 +0530

    weather_forecasting

--- weather_forecasting.py
+++ weather_forecasting.py
@@ -1,20 +1,25 @@
 import math
-
-def check(temp,humid,wind):
-    prediction=(0.5*(temp*temp))-(0.2*(humid))+(0.1*(wind))-15
-    print(prediction)
+def call(prediction):
     if prediction>=300:
-        print("It is Sunny")
+        return "It is Sunny"
     elif prediction>200 and prediction<=300:
-        print("It is Cloudy")
+        return "It is Cloudy"
     elif prediction>100 and prediction<200:
-        print("It is Rainy")
+        return "It is Rainy"
     elif prediction<=100:
-        print("It is Stormy")
-
-
-#driver 
-temp=int(input("Enter temp :"))
-humid=int(input("Enter humidity:"))
-wind=int(input("Enter wind speed:"))
-check(temp,humid,wind)
+        return "It is Stromy"
+def check(temp,humid,wind):
+    prediction=(0.5*math.pow(temp,2))-(0.2*(humid))+(0.1*(wind))-15
+    result=call(prediction)
+    return result
+        
+#driver
+if __name__ == "__main__":
+    temp=int(input("Enter temp :"))
+    humid=int(input("Enter humidity:"))
+    wind=int(input("Enter wind speed:"))
+    final=check(temp,humid,wind)
+    input_file="/Users/shashwat/Desktop/practice problems /check.txt"
+    f = open(input_file, "r")
+    print(f.read(),final)
+    
